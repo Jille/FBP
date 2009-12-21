68ad9a22e14c6ce8a83d1d2e0beb09951c586492
Sjors Gielen <sjors@sjorsgielen.nl>
2009-12-21 03:46:13 +0000

    Added RequestPacket

--- common/fbp.h
+++ common/fbp.h
@@ -22,7 +22,15 @@ struct Announcement
   char checksum[32];    // complete SHA1 checksum of the file
 } __attribute__((__packed__));
 
-struct DataPacket {
+struct RequestPacket
+{
+  char fileid;          // ID of the file (must be > 0)
+  pkt_count offset;     // first packet we want to receive
+  pkt_count num;        // number of packets we want to receive
+} __attribute__((__packed__));
+
+struct DataPacket
+{
   char fileid;          // ID of the file (must be > 0)
   char unused1;         // unused
   char unused2;         // unused
