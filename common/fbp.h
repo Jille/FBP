cc82b78a2e9a366fe1c51047ce9ef5a948e6bda1
Jille Timmermans <jille@quis.cx>
2009-12-21 03:08:29 +0000

    pkt_count geintroduceert

--- common/fbp.h
+++ common/fbp.h
@@ -9,13 +9,15 @@
 #define	FBP_STATUS_WAITING	0
 #define	FBP_STATUS_TRANSFERRING	1
 
+typedef uint32_t pkt_count;
+
 struct Announcement
 {
   char zero;          // ALWAYS 0, means this is an announcement packet
   char announceVer;   // ALWAYS 1, means this is version 1 of the announcement
   char fileid;        // ID of the file
   char status;        // 0=waiting, 1=transferring
-  uint32_t numPackets; // 4 bytes: number of packets
+  pkt_count numPackets; // 4 bytes: number of packets
   char filename[32];  // 32 bytes of filename
 } __attribute__((__packed__));
 
