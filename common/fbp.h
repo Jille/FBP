7901802adb1720fb06617e1beb8ce5102dfbca7c
Sjors Gielen <sjors@sjorsgielen.nl>
2009-12-21 03:38:36 +0000

    Filename is now 256 bytes, also added a 32 byte checksum to the announcements; added comments to struct DataPacket

--- common/fbp.h
+++ common/fbp.h
@@ -3,31 +3,32 @@
 
 #include <inttypes.h>
 
-#define	FBP_DEFAULT_PORT	1026
-#define	FBP_PACKET_DATASIZE	4096
-#define	FBP_ANNOUNCE_VERSION	1
-#define	FBP_STATUS_WAITING	0
-#define	FBP_STATUS_TRANSFERRING	1
+#define FBP_DEFAULT_PORT        1026
+#define FBP_PACKET_DATASIZE     4096
+#define FBP_ANNOUNCE_VERSION    1
+#define FBP_STATUS_WAITING      0
+#define FBP_STATUS_TRANSFERRING 1
 
 typedef uint32_t pkt_count;
 
 struct Announcement
 {
-  char zero;          // ALWAYS 0, means this is an announcement packet
-  char announceVer;   // ALWAYS 1, means this is version 1 of the announcement
-  char fileid;        // ID of the file
-  char status;        // 0=waiting, 1=transferring
+  char zero;            // ALWAYS 0, means this is an announcement packet
+  char announceVer;     // ALWAYS 1, means this is version 1 of the announcement
+  char fileid;          // ID of the file (must be > 0)
+  char status;          // 0=waiting, 1=transferring
   pkt_count numPackets; // 4 bytes: number of packets
-  char filename[32];  // 32 bytes of filename
+  char filename[256];   // 256 bytes of filename
+  char checksum[32];    // complete SHA1 checksum of the file
 } __attribute__((__packed__));
 
-struct dataPacket {
-	char fileid;
-	char unused1;
-	char unused2;
-	char checksum[5];
-	pkt_count offset;
-	char data[FBP_PACKET_DATASIZE];
+struct DataPacket {
+  char fileid;          // ID of the file (must be > 0)
+  char unused1;         // unused
+  char unused2;         // unused
+  char checksum[5];     // checksum of this packet
+  pkt_count offset;     // offset number of this packet
+  char data[FBP_PACKET_DATASIZE]; // the actual data
 } __attribute__((__packed__));
 
 #endif // FBP_GLOBAL_H
