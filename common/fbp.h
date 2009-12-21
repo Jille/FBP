f8146c164816367789d2b6cc141a7f37a68cac7f
Sjors Gielen <sjors@sjorsgielen.nl>
2009-12-21 02:32:24 +0000

    Add fileid, remove unused2, change order

--- common/fbp.h
+++ common/fbp.h
@@ -10,8 +10,8 @@ struct Announcement
 {
   char zero;          // ALWAYS 0, means this is an announcement packet
   char announceVer;   // ALWAYS 1, means this is version 1 of the announcement
+  char fileid;        // ID of the file
   char status;        // 0=not sending, 1=sending
-  char unused2;       // unused
   quint32 numPackets; // 4 bytes: number of packets
   char filename[32];  // 32 bytes of filename
 } __attribute__((__packed__));
