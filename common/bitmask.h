c9612fb5f26acc8df528d71f1a7f487ae6346683
Jille Timmermans <jille@quis.cx>
2009-12-21 04:06:50 +0000

    BM_FREE gemaakt

--- common/bitmask.h
+++ common/bitmask.h
@@ -1,5 +1,6 @@
 #define	BM_DEFINE(m)	int *m
 #define	BM_INIT(m, numbits)	m = calloc(1, numbits / sizeof(int))
-#define BM_SET(m, n) m[n/sizeof(int)] |= (1 << (n % sizeof(int)))
-#define BM_CLR(m, n) m[n/sizeof(int)] &= ~(1 << (n % sizeof(int)))
-#define BM_ISSET(m, n) ((m[n/sizeof(int)] & (1 << (n % sizeof(int)))) != 0)
+#define	BM_SET(m, n) m[n/sizeof(int)] |= (1 << (n % sizeof(int)))
+#define	BM_CLR(m, n) m[n/sizeof(int)] &= ~(1 << (n % sizeof(int)))
+#define	BM_ISSET(m, n) ((m[n/sizeof(int)] & (1 << (n % sizeof(int)))) != 0)
+#define	BM_FREE(m)	free(m)
