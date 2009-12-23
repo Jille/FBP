8307431d1278439c5c323d1539d80cb92e208ce6
Jille Timmermans <jille@quis.cx>
2009-12-23 00:51:45 +0000

    Minder falende versie van de bitmask-code

--- common/bitmask.h
+++ common/bitmask.h
@@ -1,7 +1,9 @@
-#define BM_DEFINE(m)        int *m
-#define BM_SIZE(numbits)    numbits / sizeof(int)
-#define BM_INIT(m, numbits) m = (int*)calloc(1, numbits / sizeof(int))
-#define BM_SET(m, n)        m[n/sizeof(int)] |= (1 << (n % sizeof(int)))
-#define BM_CLR(m, n)        m[n/sizeof(int)] &= ~(1 << (n % sizeof(int)))
-#define BM_ISSET(m, n)      ((m[n/sizeof(int)] & (1 << (n % sizeof(int)))) != 0)
+typedef int bm_datatype;
+
+#define BM_DEFINE(m)        bm_datatype *m
+#define BM_SIZE(numbits)    (((numbits) + ((sizeof(bm_datatype)*8) - 1)) / (sizeof(bm_datatype)*8))
+#define BM_INIT(m, numbits) m = calloc(BM_SIZE(numbits), sizeof(bm_datatype))
+#define BM_SET(m, n)        m[n/sizeof(bm_datatype)] |= (1 << (n % sizeof(bm_datatype)))
+#define BM_CLR(m, n)        m[n/sizeof(bm_datatype)] &= ~(1 << (n % sizeof(bm_datatype)))
+#define BM_ISSET(m, n)      ((m[n/sizeof(bm_datatype)] & (1 << (n % sizeof(bm_datatype)))) != 0)
 #define BM_FREE(m)          free(m)
