fb98e74432869006021a64ba039531dbc6c09169
Jille Timmermans <jille@quis.cx>
2009-12-26 02:27:50 +0000

    bm_find_setbit en bm_find_clrbit toegevoegd

--- common/bitmask.h
+++ common/bitmask.h
@@ -1,4 +1,5 @@
 typedef int bm_datatype;
+typedef int bm_bitid;
 
 #define BM_DEFINE(m)        bm_datatype *m
 #define BM_SIZE(numbits)    (((numbits) + ((sizeof(bm_datatype)*8) - 1)) / (sizeof(bm_datatype)*8))
@@ -7,3 +8,29 @@ typedef int bm_datatype;
 #define BM_CLR(m, n)        m[n/sizeof(bm_datatype)] &= ~(1 << (n % sizeof(bm_datatype)))
 #define BM_ISSET(m, n)      ((m[n/sizeof(bm_datatype)] & (1 << (n % sizeof(bm_datatype)))) != 0)
 #define BM_FREE(m)          free(m)
+
+static inline bm_bitid
+bm_find_setbit(bm_datatype *m, bm_bitid numbits, bm_bitid offset) {
+	bm_bitid n = offset;
+	while(!BM_ISSET(m, n)) {
+		n = (n+1) % numbits;
+		if(n == offset) {
+			return -1;
+		}
+	}
+
+	return n;
+}
+
+static inline bm_bitid
+bm_find_clrbit(bm_datatype *m, bm_bitid numbits, bm_bitid offset) {
+	bm_bitid n = offset;
+	while(BM_ISSET(m, n)) {
+		n = (n+1) % numbits;
+		if(n == offset) {
+			return -1;
+		}
+	}
+
+	return n;
+}
