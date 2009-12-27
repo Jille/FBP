7815c4995d778f9461da757311e66da800165d03
Jille Timmermans <jille@quis.cx>
2009-12-27 02:22:39 +0000

    Mogelijke bugfix in bitmask.h; het lijkt geen regression te tonen (progression is voor mij ook niet duidelijk)

--- common/bitmask.h
+++ common/bitmask.h
@@ -2,12 +2,13 @@ typedef int bm_datatype;
 typedef int bm_bitid;
 
 #define BM_DEFINE(m)        bm_datatype *m
-#define BM_SIZE(numbits)    (((numbits) + ((sizeof(bm_datatype)*8) - 1)) / (sizeof(bm_datatype)*8))
+#define BM_BITS_PER_UNIT    (sizeof(bm_datatype)*8)
+#define BM_SIZE(numbits)    (((numbits) + (BM_BITS_PER_UNIT - 1)) / BM_BITS_PER_UNIT)
 // Cast is necessary to make the C++ compiler happy
 #define BM_INIT(m, numbits) m = (bm_datatype*)calloc(BM_SIZE(numbits), sizeof(bm_datatype))
-#define BM_SET(m, n)        m[n/sizeof(bm_datatype)] |= (1 << (n % sizeof(bm_datatype)))
-#define BM_CLR(m, n)        m[n/sizeof(bm_datatype)] &= ~(1 << (n % sizeof(bm_datatype)))
-#define BM_ISSET(m, n)      ((m[n/sizeof(bm_datatype)] & (1 << (n % sizeof(bm_datatype)))) != 0)
+#define BM_SET(m, n)        m[n/BM_BITS_PER_UNIT] |= (1 << (n % BM_BITS_PER_UNIT))
+#define BM_CLR(m, n)        m[n/BM_BITS_PER_UNIT] &= ~(1 << (n % BM_BITS_PER_UNIT))
+#define BM_ISSET(m, n)      ((m[n/BM_BITS_PER_UNIT] & (1 << (n % BM_BITS_PER_UNIT))) != 0)
 #define BM_FREE(m)          free(m)
 
 static inline bm_bitid
